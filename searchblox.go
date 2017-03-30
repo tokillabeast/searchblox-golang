16e8de096a3d9ae56940a1899e516ad87b9d39ae
tokillamockingbird <tokillamockingbird1989@gmail.com>
2017-03-30 13:18:37 +0300

    Support all Rest API call, but for now without testing

--- searchblox.go
+++ searchblox.go
@@ -39,6 +39,31 @@ type Client struct {
 	// FIXME: store ApiKey in Client struct?
 }
 
+type CustomCollection struct {
+	ApiKey            string             `json:"apikey"`
+	Document          *Document          `json:"document,omitempty"`
+	ColName           string             `json:"colname,omitempty"`
+	ColType           string             `json:"coltype,omitempty"`
+	RootUrls          []string           `json:"rooturls,omitempty"`
+	AllowPaths        []string           `json:"allowpaths,omitempty"`
+	DisallowPaths     []string           `json:"disallowpaths,omitempty"`
+	AllowFormat       []string           `json:"allowformat,omitempty"`
+	KeywordInContext  string             `json:"keyword-in-context,omitempty"`
+	RemoveDuplicates  string             `json:"remove-duplicates,omitempty"`
+	Boost             string             `json:"boost,omitempty"`
+	Stemming          string             `json:"stemming,omitempty"`
+	Spelling          string             `json:"spelling,omitempty"`
+	Logging           string             `json:"logging,omitempty"`
+	HtmlSettings      *HtmlSettings      `json:"html-settings,omitempty"`
+	BasicAuthSettings *BasicAuthSettings `json:"basic-auth-settings,omitempty"`
+	FormAuthSettings  *FormAuthSettings  `json:"form-auth-settings,omitempty"`
+	ProxySettings     *ProxySettings     `json:"proxy-settings,omitempty"`
+	Index             *Scheduler         `json:"index,omitempty"`
+	Clear             *Scheduler         `json:"clear,omitempty"`
+	Refresh           *Scheduler         `json:"refresh,omitempty""`
+	Action            string             `json:"action,omitempty"`
+}
+
 type Document struct {
 	ColName      string            `json:"colname"`
 	Url          string            `json:"url,omitempty"`
@@ -55,14 +80,49 @@ type Document struct {
 	Meta         map[string]string `json:"meta"`
 }
 
-type CustomCollection struct {
-	ApiKey   string   `json:"apikey"`
-	Document Document `json:"document"`
+type HtmlSettings struct {
+	Description    string `json:"description,omitempty"`
+	MaxDocAge      string `json:"max-doc-age,omitempty"`
+	MaxDocSize     string `json:"max-doc-size,omitempty"`
+	SpiderMaxDepth string `json:"spider-max-depth,omitempty"`
+	SpiderMaxDelay string `json:"spider-max-delay,omitempty"`
+	UserAgent      string `json:"user-agent,omitempty"`
+	Referer        string `json:"referer,omitempty"`
+	IgnoreRobots   string `json:"ignore-robots,omitempty"`
+	FollowSitemap  string `json:"follow-sitemap,omitempty"`
+	FollowRedirect string `json:"follow-redirect,omitempty"`
+}
+
+type BasicAuthSettings struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+type FormAuthSettings struct {
+	FormUrl    string `json:"form-url,omitempty"`
+	FormAction string `json:"form-action,omitempty"`
+	Form       []Form `json:"form,omitempty"`
+}
+
+type Form struct {
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type ProxySettings struct {
+	ServerUrl string `json:"server-url,omitempty"`
+	Username  string `json:"username,omitempty"`
+	Password  string `json:"password,omitempty"`
+}
+
+type Scheduler struct {
+	Frequency string `json:"frequency,omitemtpy"` // FIXME: Do we need to omit these values for all nested structs?
+	TimeStamp string `json:"timestamp,omitemtpy"`
 }
 
 //FIXME: better searchblox exception handler(Bad request, etc)
-func (s *Client) makeCall(url string, customCollection CustomCollection) ([]byte, error) {
-	b, err := json.Marshal(customCollection)
+func (s *Client) makeCall(url string, collection CustomCollection) ([]byte, error) {
+	b, err := json.Marshal(collection)
 	if err != nil {
 		return nil, errors.New(encodeErrorJSON)
 	}
@@ -83,45 +143,128 @@ func (s *Client) makeCall(url string, customCollection CustomCollection) ([]byte
 	return body, nil
 }
 
-func (s *Client) CreateCustomCollection(customCollection CustomCollection) (string, error) {
+func (s *Client) CreateCustomCollection(collection CustomCollection) (string, error) {
 	url := fmt.Sprintf("%s%s", s.Host, createCustomCollectionJSON)
-	body, err := s.makeCall(url, customCollection)
+	body, err := s.makeCall(url, collection)
 	if err != nil {
 		return "", err
 	}
 	return string(body), nil
 }
 
-func (s *Client) DeleteCustomCollection(customCollection CustomCollection) (string, error) {
+func (s *Client) DeleteCustomCollection(collection CustomCollection) (string, error) {
 	url := fmt.Sprintf("%s%s", s.Host, deleteCustomCollectionJSON)
-	body, err := s.makeCall(url, customCollection)
+	body, err := s.makeCall(url, collection)
 	if err != nil {
 		return "", err
 	}
 	return string(body), nil
 }
 
-func (s *Client) ClearCustomCollection(customCollection CustomCollection) (string, error) {
+func (s *Client) ClearCustomCollection(collection CustomCollection) (string, error) {
 	url := fmt.Sprintf("%s%s", s.Host, clearCustomCollectionJSON)
-	body, err := s.makeCall(url, customCollection)
+	body, err := s.makeCall(url, collection)
 	if err != nil {
 		return "", err
 	}
 	return string(body), nil
 }
 
-func (s *Client) IndexDocumentCustomCollection(customCollection CustomCollection) (string, error) {
+func (s *Client) IndexDocumentCustomCollection(collection CustomCollection) (string, error) {
 	url := fmt.Sprintf("%s%s", s.Host, indexDocumentCustomCollectionJSON)
-	body, err := s.makeCall(url, customCollection)
+	body, err := s.makeCall(url, collection)
 	if err != nil {
 		return "", err
 	}
 	return string(body), nil
 }
 
-func (s *Client) DocumentStatusCustomCollection(customCollection CustomCollection) (string, error) { //FIXME: return map to support meta?
+func (s *Client) DocumentStatusCustomCollection(collection CustomCollection) (string, error) {
 	url := fmt.Sprintf("%s%s", s.Host, documentStatusCustomCollectionJSON)
-	body, err := s.makeCall(url, customCollection)
+	body, err := s.makeCall(url, collection)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (s *Client) DeleteDocumentCustomCollection(collection CustomCollection) (string, error) {
+	url := fmt.Sprintf("%s%s", s.Host, deleteDocumentCustomCollectionJSON)
+	body, err := s.makeCall(url, collection)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (s *Client) AddUpdateDocumentHttpFileSystemCollection(collection CustomCollection) (string, error) {
+	url := fmt.Sprintf("%s%s", s.Host, addUpdateDocumentHttpFileSystemCollectionJSON)
+	body, err := s.makeCall(url, collection)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (s *Client) DeleteDocumentHttpFileSystemCollection(collection CustomCollection) (string, error) {
+	url := fmt.Sprintf("%s%s", s.Host, deleteDocumentHttpFileSystemCollectionJSON)
+	body, err := s.makeCall(url, collection)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (s *Client) AddHttpFileSystemOrDatabaseCollection(collection CustomCollection) (string, error) {
+	url := fmt.Sprintf("%s%s", s.Host, addHttpFileSystemOrDatabaseCollectionJSON)
+	body, err := s.makeCall(url, collection)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (s *Client) DeleteHttpFileSystemOrDatabaseCollection(collection CustomCollection) (string, error) {
+	url := fmt.Sprintf("%s%s", s.Host, deleteHttpFileSystemOrDatabaseCollectionJSON)
+	body, err := s.makeCall(url, collection)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (s *Client) UpdatePathHttpFileSystemOrDatabaseCollection(collection CustomCollection) (string, error) {
+	url := fmt.Sprintf("%s%s", s.Host, updatePathHttpFileSystemOrDatabaseCollectionJSON)
+	body, err := s.makeCall(url, collection)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (s *Client) UpdateSettingsHttpFileSystemOrDatabaseCollection(collection CustomCollection) (string, error) {
+	url := fmt.Sprintf("%s%s", s.Host, updateSettingsHttpFileSystemOrDatabaseCollectionJSON)
+	body, err := s.makeCall(url, collection)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (s *Client) UpdateSchedulerHttpFileSystemOrDatabaseCollection(collection CustomCollection) (string, error) {
+	url := fmt.Sprintf("%s%s", s.Host, updateSchedulerHttpFileSystemOrDatabaseCollectionJSON)
+	body, err := s.makeCall(url, collection)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+// FIXME: more simple titles? Maybe IndexStopCrawler and comments for this method
+// INDEX/STOP CRAWLER - HTTP, FILESYSTEM OR DATABASE COLLECTION
+func (s *Client) IndexStopCrawlerHttpFileSystemOrDatabaseCollection(collection CustomCollection) (string, error) {
+	url := fmt.Sprintf("%s%s", s.Host, indexDocumentCustomCollectionJSON)
+	body, err := s.makeCall(url, collection)
 	if err != nil {
 		return "", err
 	}
